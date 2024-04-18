0864f586054059c4760ab4c14bc3dbe44f37d538
Rory Duncan <rory.duncan@altsourcesoftware.com>
2024-04-18 00:27:32 -0700

    Use open-props

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	/* the props */
+	// import "open-props/index.css";
+	import "open-props/src/index.css";
+
+	/* optional imports that use the props */
+	import "open-props/src/extra/normalize.css";
+	import "open-props/src/extra/buttons.css";
+
+</script>
+
+<slot />
+
+<style>
+    :root {
+		--link: #cc7633;
+		--link-visited: #a2581f;
+	}
+</style>
